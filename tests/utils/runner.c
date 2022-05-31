fb1ceabf931276b4e91405bcb24e79fc6f3da497
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-05-31 18:36:22 -0300

    feat: add test runner

---
+++ tests/utils/runner.c
@@ -0,0 +1,62 @@
+#include "runner.h"
+
+t_unit_test *g_test;
+
+void	test_assert(int condition, int line)
+{
+	if (condition == true)
+		g_test->failed = false;
+	else
+		g_test->line = line;
+}
+
+void	test_runner(t_unit_test *test)
+{
+	g_test = test;
+	g_test->failed = true;
+	test->func();
+}
+
+int		run_units(t_unit_test tests[], int n_tests, char *filename)
+{
+	int		fails;
+
+	fails = 0;
+	for (int i = 0; i < n_tests; i++)
+	{
+		test_runner(&tests[i]);
+		if (tests[i].failed)
+			fails++;
+	}
+	t_session session = {
+		.fails = fails,
+		.tests = tests,
+		.n_tests = n_tests,
+		.filename = filename,
+	};
+
+	// p_result(session);
+	print_result(&session);
+	return (EXIT_SUCCESS);
+}
+
+// TODO: no modo compacto, exibir só o nome da função: ft_isalpha
+void	p_res(t_unit_test *test)
+{
+	if (test->failed)
+		printf("%s %s%s at line %d%s\n", FAIL_SIGN, RED, test->name, test->line, RESET);
+	else
+		printf("%s %s\n", PASS_SIGN, test->name);
+}
+
+void	print_result(t_session *s)
+{
+	if (s->fails)
+		printf("\n%s %s\n", FAILED, s->filename);
+	else
+		printf("\n%s %s\n", PASSED, s->filename);
+	for (int i = 0; i < s->n_tests; i++)
+	{
+		p_res(&s->tests[i]);
+	}
+}
---
+++ tests/utils/runner.h
@@ -0,0 +1,52 @@
+#ifndef RUNNER_H
+# define RUNNER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
+
+# define RED					"\033[31m"
+# define RESET					"\033[m"
+# define BOLD(string)			"\033[1m" string "\033[0m"
+# define BG_RED(string)			"\033[7;31m" string "\033[0m"
+# define FG_RED(string)			"\033[1;31m" string "\033[0m"
+# define BG_GREEN(string)		"\033[7;32m" string "\033[0m"
+# define FG_GREEN(string)		"\033[1;32m" string "\033[0m"
+
+# define FAILED					BG_RED(" FAIL ")
+# define PASSED					BG_GREEN(" PASS ")
+
+# define FAIL_SIGN				FG_RED("  •")
+# define PASS_SIGN				FG_GREEN("  ✓")
+
+# define RUN_GROUP(tests)		run_units(tests, sizeof(tests)/sizeof(tests[0]), __FILE__)
+# define UNIT_TEST(function)	{.func=function, .name=#function}
+# define ASSERT_EXPR(condition)	test_assert(condition, __LINE__)
+
+typedef struct	s_unit_test
+{
+	char		*name;
+	void		(*func)(void);
+	int			line;
+	int			failed;
+}				t_unit_test;
+
+typedef struct	s_session
+{
+	int			fails;
+	t_unit_test	*tests;
+	int			n_tests;
+	char		*filename;
+}				t_session;
+
+void	test_assert(int condition, int line);
+void	test_runner(t_unit_test *new_test);
+int		run_units(t_unit_test tests[], int num_tests, char *filename);
+
+void	print_result(t_session *s);
+// void	print_result(t_session session);
+void	print_pass(t_session session);
+void	print_fail(t_session session);
+
+#endif
