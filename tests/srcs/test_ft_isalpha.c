6b11fc57f6ab02a85b05d708a54ea1d01094e4a4
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-01 18:00:23 -0300

    test: ensure ft_isalpha handles non-printable chars

--- tests/srcs/test_ft_isalpha.c
+++ tests/srcs/test_ft_isalpha.c
@@ -11,10 +11,21 @@ static void	test_printable_character(void)
 	ASSERT_EXPR(result && expected);
 }
 
+static void	test_non_printable_character(void)
+{
+	int	result;
+	int	expected;
+
+	result = ft_isalpha(27);
+	expected = isalpha(27);
+	ASSERT_EXPR(result == expected);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_printable_character),
+		UNIT_TEST(test_non_printable_character),
 	};
 	RUN_GROUP(tests);
 }
