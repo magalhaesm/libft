e68baf2cfeccf53ec2bc0c77653ac47482097e13
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-02 08:27:56 -0300

    test: ensure ft_tolower handles lowercase chars

--- tests/srcs/test_ft_tolower.c
+++ tests/srcs/test_ft_tolower.c
@@ -1,7 +1,7 @@
 #include "../utils/tester.h"
 #include "ctype.h"
 
-static void	test_uppercase_character(void)
+void	test_uppercase_character(void)
 {
 	int	result;
 	int	expected;
@@ -11,10 +11,21 @@ static void	test_uppercase_character(void)
 	ASSERT_EXPR(result == expected);
 }
 
+void	test_lowercase_character(void)
+{
+	int	result;
+	int	expected;
+
+	result = ft_tolower('a');
+	expected = tolower('a');
+	ASSERT_EXPR(result == expected);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_uppercase_character),
+		UNIT_TEST(test_lowercase_character),
 	};
 	return RUN_GROUP(tests);
 }
