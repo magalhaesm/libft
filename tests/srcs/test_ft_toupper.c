7e3e75801a7e33d12c984bc4e4a52a9f5b355a5d
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-01 21:06:16 -0300

    test: ensure ft_toupper handles lowercase chars

--- tests/srcs/test_ft_toupper.c
+++ tests/srcs/test_ft_toupper.c
@@ -11,10 +11,21 @@ static void	test_uppercase_character(void)
 	ASSERT_EXPR(result == expected);
 }
 
+static void	test_lowercase_character(void)
+{
+	int	result;
+	int	expected;
+
+	result = ft_toupper('a');
+	expected = toupper('a');
+	ASSERT_EXPR(result == expected);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_uppercase_character),
+		UNIT_TEST(test_lowercase_character),
 	};
 	RUN_GROUP(tests);
 }
