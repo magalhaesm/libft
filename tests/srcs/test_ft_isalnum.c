94555ba16a5024e2c027108fea819584c399b322
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-02 22:49:18 -0300

    test: ensure ft_isalnum handles numeric chars

--- tests/srcs/test_ft_isalnum.c
+++ tests/srcs/test_ft_isalnum.c
@@ -11,10 +11,21 @@ void	test_alphabetic_character(void)
 	ASSERT_EXPR(result && expected);
 }
 
+void	test_numeric_character(void)
+{
+	int	result;
+	int	expected;
+
+	result = ft_isalnum('0');
+	expected = isalnum('0');
+	ASSERT_EXPR(result && expected);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_alphabetic_character),
+		UNIT_TEST(test_numeric_character),
 	};
 	return RUN_GROUP(tests);
 }
