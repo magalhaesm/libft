7bd9364fdd374e015f16847199d66267f60ef212
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-03 13:59:59 -0300

    test: ensure ft_isprint handles non-printable chars

--- tests/srcs/test_ft_isprint.c
+++ tests/srcs/test_ft_isprint.c
@@ -11,10 +11,21 @@ void	test_printable_character(void)
 	ASSERT_EXPR(result && expected);
 }
 
+void	test_non_printable_character(void)
+{
+	int	result;
+	int	expected;
+
+	result = ft_isprint(127);
+	expected = isprint(127);
+	ASSERT_EXPR(result == expected);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_printable_character),
+		UNIT_TEST(test_non_printable_character),
 	};
 	return RUN_GROUP(tests);
 }
