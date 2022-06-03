cf972beab6f03566863d9468b9e6b1bea17ab8a8
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-03 13:51:21 -0300

    feat: add ft_isprint

--- libft/ft_isprint.c
+++ libft/ft_isprint.c
@@ -1,24 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/30 22:41:44 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/06/03 13:47:00 by mdias-ma         ###   ########.fr       */
+/*   Created: 2022/06/03 13:47:09 by mdias-ma          #+#    #+#             */
+/*   Updated: 2022/06/03 13:50:50 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(int c);
-int	ft_toupper(int c);
-int	ft_isdigit(int c);
-int	ft_tolower(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-#endif
+int	ft_isprint(int c)
+{
+	return (c >= 32 && c <= 127);
+}
