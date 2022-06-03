a1b9064f80df153d9f0584142996a354b390b13b
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-03 13:36:36 -0300

    test: ensure ft_isascii handles non-ascii chars

--- tests/srcs/test_ft_isascii.c
+++ tests/srcs/test_ft_isascii.c
@@ -11,10 +11,21 @@ void	test_ascii_character(void)
 	ASSERT_EXPR(result && expected);
 }
 
+void	test_non_ascii_character(void)
+{
+	int	result;
+	int	expected;
+
+	result = ft_isascii(227); // 227 = ã
+	expected = isascii(227);
+	ASSERT_EXPR((result && expected) == 0);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_ascii_character),
+		UNIT_TEST(test_non_ascii_character),
 	};
 	return RUN_GROUP(tests);
 }
