e2b5eac12e83f6c3f29e89de60fbdbf8965e99a4
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-03 13:40:24 -0300

    chore: remove unneeded line

--- libft/ft_isdigit.c
+++ libft/ft_isdigit.c
@@ -6,11 +6,9 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 21:45:44 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/06/01 21:50:15 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/06/02 16:00:50 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
 
 int	ft_isdigit(int c)
 {
