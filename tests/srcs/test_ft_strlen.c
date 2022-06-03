de5077a1430322dd167d2cc68b2f640a55c53c59
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-03 16:27:51 -0300

    test: ensure ft_strlen handles non-ascii strings

--- tests/srcs/test_ft_strlen.c
+++ tests/srcs/test_ft_strlen.c
@@ -23,11 +23,23 @@ void	test_ascii_character_string(void)
 	ASSERT_EXPR(result == expected);
 }
 
+void	test_non_ascii_character_string(void)
+{
+	char	test[] = {-10, 227, 252, '\0'}; // 246, ã, ü, \n
+	int		result;
+	int		expected;
+
+	result = ft_strlen(test);
+	expected = strlen(test);
+	ASSERT_EXPR(result == expected);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_empty_string),
 		UNIT_TEST(test_ascii_character_string),
+		UNIT_TEST(test_non_ascii_character_string),
 	};
 	return RUN_GROUP(tests);
 }
