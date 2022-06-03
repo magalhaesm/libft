79d96362a12a2d117f74bed5f9e8b766ac3c6b73
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-03 13:23:17 -0300

    feat: add ft_isascii

--- libft/ft_isascii.c
+++ libft/ft_isascii.c
@@ -1,23 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/30 22:41:44 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/06/02 23:25:18 by mdias-ma         ###   ########.fr       */
+/*   Created: 2022/06/02 23:22:42 by mdias-ma          #+#    #+#             */
+/*   Updated: 2022/06/03 13:01:59 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(int c);
-int	ft_toupper(int c);
-int	ft_isdigit(int c);
-int	ft_tolower(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-
-#endif
+int	ft_isascii(int c)
+{
+	return ((unsigned char)c <= 127);
+}
