fca5ab88a164f761f8160d65645acc3d41d9e9b6
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-03 20:10:38 -0300

    feat: add ft_strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/30 22:41:44 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/06/03 17:46:25 by mdias-ma         ###   ########.fr       */
+/*   Created: 2022/06/03 17:34:46 by mdias-ma          #+#    #+#             */
+/*   Updated: 2022/06/03 17:44:54 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-# include <stddef.h>
-
-int		ft_isalpha(int c);
-int		ft_toupper(int c);
-int		ft_isdigit(int c);
-int		ft_tolower(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
-
-#endif
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != '\0' && *s != c)
+		s++;
+	return ((char *)s);
+}
