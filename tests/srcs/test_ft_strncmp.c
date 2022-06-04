280ac25961492e6d1148989a1f7a5b8cda795298
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-04 11:26:15 -0300

    test: ensure ft_strncmp compares non-ascii strings

--- tests/srcs/test_ft_strncmp.c
+++ tests/srcs/test_ft_strncmp.c
@@ -6,8 +6,8 @@ void	test_ascii_string_are_equal(void)
 	char	*s1 = "ABC";
 	char	*s2 = "AB";
 	int		result = ft_strncmp(s1, s2, 2);
-	int		expeted = strncmp(s1, s2, 2);
-	ASSERT_EXPR(result == expeted);
+	int		expected = strncmp(s1, s2, 2);
+	ASSERT_EXPR(result == expected);
 }
 
 void	test_ascii_string_are_different(void)
@@ -15,8 +15,17 @@ void	test_ascii_string_are_different(void)
 	char	*s1 = "ABC";
 	char	*s2 = "AB";
 	int		result = ft_strncmp(s1, s2, 3);
-	int		expeted = strncmp(s1, s2, 3);
-	ASSERT_EXPR(result == expeted);
+	int		expected = strncmp(s1, s2, 3);
+	ASSERT_EXPR(result == expected);
+}
+
+void	test_non_ascii_string_are_different(void)
+{
+	char	s1[] = {'A', 'B', -10};
+	char	*s2 = "AB";
+	int		result = ft_strncmp(s1, s2, 3);
+	int		expected = strncmp(s1, s2, 3);
+	ASSERT_EXPR(result == expected);
 }
 
 int	main(void)
@@ -24,6 +33,7 @@ int	main(void)
 	t_unit_test tests[] = {
 		UNIT_TEST(test_ascii_string_are_equal),
 		UNIT_TEST(test_ascii_string_are_different),
+		UNIT_TEST(test_non_ascii_string_are_different),
 	};
 	return RUN_GROUP(tests);
 }
