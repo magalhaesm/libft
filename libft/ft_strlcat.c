b684619b2474441f401f5aa50584360322a53cd4
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-05 17:41:24 -0300

    test: ensure ft_strlcat concatenates strings without enough space

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 13:47:48 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/06/05 14:56:43 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/06/05 16:34:10 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	int	i;
-	int	j;
+	size_t	i;
+	size_t	j;
 
 	i = ft_strlen(dst);
 	j = 0;
-	while (src[j] != '\0')
+	while (src[j] != '\0' && i < size - 1)
 		dst[i++] = src[j++];
 	dst[i] = '\0';
 	return (size);
--- tests/srcs/test_ft_strlcat.c
+++ tests/srcs/test_ft_strlcat.c
@@ -13,10 +13,21 @@ void	test_concatenate_string_having_enough_space(void)
 	ASSERT_EXPR(strcmp(result, expected) == 0);
 }
 
+void	test_concatenate_string_without_enough_space(void)
+{
+	const char	test[] = " ft_strlcat";
+	char	result[15] = "testing";
+	char	expected[15] = "testing";
+	ft_strlcat(result, test, 15);
+	strlcat(expected, test, 15);
+	ASSERT_EXPR(strcmp(result, expected) == 0);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_concatenate_string_having_enough_space),
+		UNIT_TEST(test_concatenate_string_without_enough_space),
 	};
 	return RUN_GROUP(tests);
 }
