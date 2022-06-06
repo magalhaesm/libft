b418e50c2e03e9daa1a7416ad4802c0afe4d9105
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-06 14:41:55 -0300

    test: ensure ft_strnstr handles empty strings to search in

--- tests/srcs/test_ft_strnstr.c
+++ tests/srcs/test_ft_strnstr.c
@@ -37,12 +37,25 @@ void	test_empty_searched_string(void)
 	ASSERT_EXPR(result == expected);
 }
 
+void	test_return_null_when_search_in_a_empty_string(void)
+{
+	const char	*large = "";
+	const char	*small = "Bar";
+	char	*result;
+	char	*expected;
+
+	result = ft_strnstr(large, small, 5);
+	expected = strnstr(large, small, 5);
+	ASSERT_EXPR(result == expected);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_locate_the_first_occurrence_of_a_char),
 		UNIT_TEST(test_locate_the_first_occurrence_of_a_string),
 		UNIT_TEST(test_empty_searched_string),
+		UNIT_TEST(test_return_null_when_search_in_a_empty_string),
 	};
 	return RUN_GROUP(tests);
 }
