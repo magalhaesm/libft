6890f58314a9fa9a1ad4b3b108829d4d3496436d
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-08 17:31:24 -0300

    test: ensure ft_memmove copies areas that overlap

--- tests/srcs/test_ft_memmove.c
+++ tests/srcs/test_ft_memmove.c
@@ -12,10 +12,22 @@ void	test_copy_to_memory_area_without_overlap(void)
 	ASSERT_EXPR(strcmp(result, expected) == 0);
 }
 
+void	test_copy_to_memory_area_that_overlap(void)
+{
+	char	test[] = "This is an example";
+	char	*result;
+	char	*expected;
+
+	result = ft_memmove(test + 7, test, 10);
+	expected = memmove(test + 7, test, 10);
+	ASSERT_EXPR(strcmp(result, expected) == 0);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_copy_to_memory_area_without_overlap),
+		UNIT_TEST(test_copy_to_memory_area_that_overlap),
 	};
 	return RUN_GROUP(tests);
 }
