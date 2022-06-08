647093c105aaa921acd010e5904baec42fd5e1ae
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-08 18:06:29 -0300

    test: ensure ft_memchr locates a string that doesnt exist

--- tests/srcs/test_ft_memchr.c
+++ tests/srcs/test_ft_memchr.c
@@ -12,10 +12,22 @@ void	test_search_for_a_character_that_exists(void)
 	ASSERT_EXPR(result == expected);
 }
 
+void	test_search_for_a_character_that_dont_exists(void)
+{
+	char	*test = "string de teste";
+	char	*result;
+	char	*expected;
+
+	result = ft_memchr(test, 'D', 10);
+	expected = memchr(test, 'D', 10);
+	ASSERT_EXPR(result == expected);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_search_for_a_character_that_exists),
+		UNIT_TEST(test_search_for_a_character_that_dont_exists),
 	};
 	return RUN_GROUP(tests);
 }
