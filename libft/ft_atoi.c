d65b3c5194d5253701a7967f0596a21298b6eb50
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-09 18:13:28 -0300

    test: ensure ft_atoi converts positive integers

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 15:52:46 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/06/09 17:37:34 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/06/09 18:10:37 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,12 @@ int	ft_atoi(const char *nptr)
 
 	sign = 1;
 	total = 0;
-	if (*(nptr++) == '-' )
-		sign *= (-1);
+	if (*(nptr) == '-' || *(nptr) == '+')
+	{
+		if (*(nptr) == '-')
+			sign *= (-1);
+		nptr++;
+	}
 	while (ft_isdigit(*nptr))
 		total = *(nptr++) - '0' + (total * 10);
 	return (total * sign);
--- tests/srcs/test_ft_atoi.c
+++ tests/srcs/test_ft_atoi.c
@@ -12,10 +12,34 @@ void	test_convert_negative_signed_integers(void)
 	ASSERT_EXPR(result == expected);
 }
 
+void	test_convert_positive_signed_integers(void)
+{
+	char	*test = "+42";
+	int		result;
+	int		expected;
+
+	result = ft_atoi(test);
+	expected = atoi(test);
+	ASSERT_EXPR(result == expected);
+}
+
+void	test_convert_positive_unsigned_integers(void)
+{
+	char	*test = "42";
+	int		result;
+	int		expected;
+
+	result = ft_atoi(test);
+	expected = atoi(test);
+	ASSERT_EXPR(result == expected);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_convert_negative_signed_integers),
+		UNIT_TEST(test_convert_positive_signed_integers),
+		UNIT_TEST(test_convert_positive_unsigned_integers),
 	};
 	return RUN_GROUP(tests);
 }
