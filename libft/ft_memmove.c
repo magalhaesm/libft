7688bc9acc8015c66276dbbb5e9db8a74d2c425e
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-09 15:54:49 -0300

    refactor(ft_memmove): rename variables

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 18:23:09 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/06/08 23:48:15 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/06/09 15:54:12 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	t_byte	*dest_buf;
-	t_byte	*src_buf;
+	t_byte	*dstp;
+	t_byte	*srcp;
 
-	dest_buf = dest;
-	src_buf = (t_byte *)src;
+	dstp = dest;
+	srcp = (t_byte *)src;
 	if (src > dest)
 	{
 		while (n--)
-			*(dest_buf++) = *(src_buf++);
+			*(dstp++) = *(srcp++);
 		return (dest);
 	}
 	return (ft_memcpy(dest, src, n));
