c365ed13f0ea037cc85a6b674b121b3638353a7f
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-09 14:13:13 -0300

    fix(ft_memmove): move memory areas to both directions

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 18:23:09 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/06/08 17:01:10 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/06/08 23:48:15 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
+	t_byte	*dest_buf;
+	t_byte	*src_buf;
+
+	dest_buf = dest;
+	src_buf = (t_byte *)src;
+	if (src > dest)
+	{
+		while (n--)
+			*(dest_buf++) = *(src_buf++);
+		return (dest);
+	}
 	return (ft_memcpy(dest, src, n));
 }
--- tests/srcs/test_ft_memmove.c
+++ tests/srcs/test_ft_memmove.c
@@ -1,33 +1,35 @@
 #include "../utils/tester.h"
 #include <string.h>
 
-void	test_copy_to_memory_area_without_overlap(void)
+void	test_move_memory_area_to_the_right(void)
 {
 	char	test[] = "This is an example";
+	char	test2[] = "This is an example";
 	char	*result;
 	char	*expected;
 
-	result = ft_memmove(test, test, 10);
-	expected = memmove(test, test, 10);
+	result = ft_memmove(test + 1, test, 10);
+	expected = memmove(test2 + 1, test2, 10);
 	ASSERT_EXPR(strcmp(result, expected) == 0);
 }
 
-void	test_copy_to_memory_area_that_overlap(void)
+void	test_move_memory_area_to_the_left(void)
 {
-	char	test[] = "This is an example";
+	char	test[] = " search for a character that don't exists";
+	char	test2[] = " search for a character that don't exists";
 	char	*result;
 	char	*expected;
 
-	result = ft_memmove(test + 7, test, 10);
-	expected = memmove(test + 7, test, 10);
+	result = ft_memmove(test, test + 1, 40);
+	expected = memmove(test2, test2 + 1, 40);
 	ASSERT_EXPR(strcmp(result, expected) == 0);
 }
 
 int	main(void)
 {
 	t_unit_test tests[] = {
-		UNIT_TEST(test_copy_to_memory_area_without_overlap),
-		UNIT_TEST(test_copy_to_memory_area_that_overlap),
+		UNIT_TEST(test_move_memory_area_to_the_right),
+		UNIT_TEST(test_move_memory_area_to_the_left),
 	};
 	return RUN_GROUP(tests);
 }
