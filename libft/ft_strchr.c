eaba1b7480bf1f0085196171f29f9f502bbce4e8
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-10 15:41:11 -0300

    test(ft_strchr): test char overflow

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 17:34:46 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/06/03 20:26:53 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/06/10 15:40:24 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != '\0' && *s != c)
+	while (*s != '\0' && *s != (char)c)
 		s++;
 	if (*s == '\0' && c != '\0')
 		return (NULL);
--- tests/srcs/test_ft_strchr.c
+++ tests/srcs/test_ft_strchr.c
@@ -35,12 +35,24 @@ void	test_non_existing_char(void)
 	ASSERT_EXPR(result == expected);
 }
 
+void	test_char_overflow(void)
+{
+	char	test[] = "Lorem ipsum dolor sit amet.";
+	char	*result;
+	char	*expected;
+
+	result = ft_strchr(test, 'L' + 256);
+	expected = strchr(test, 'L' + 256);
+	ASSERT_EXPR(result == expected);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_locate_terminating_null),
 		UNIT_TEST(test_locate_a_existing_char),
 		UNIT_TEST(test_non_existing_char),
+		UNIT_TEST(test_char_overflow),
 	};
 	return RUN_GROUP(tests);
 }
