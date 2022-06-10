54df266ee65cf7f5a507cb5d4a5bc380106f3fb7
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2022-06-09 22:08:24 -0300

    test(ft_atoi): add test with isspace(3)
    adding test that will try all isspace(3) characters at the beginning followed by a signal.

--- tests/srcs/test_ft_atoi.c
+++ tests/srcs/test_ft_atoi.c
@@ -34,12 +34,24 @@ void	test_convert_positive_unsigned_integers(void)
 	ASSERT_EXPR(result == expected);
 }
 
+void	test_is_space(void)
+{
+	char	*test = "  \t\n\v\f\r+42";
+	int		result;
+	int		expected;
+
+	result = ft_atoi(test);
+	expected = atoi(test);
+	ASSERT_EXPR(result == expected);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_convert_negative_signed_integers),
 		UNIT_TEST(test_convert_positive_signed_integers),
 		UNIT_TEST(test_convert_positive_unsigned_integers),
+		UNIT_TEST(test_is_space),
 	};
 	return RUN_GROUP(tests);
 }
