b81100eae9b8a70b272ab81b5f70a8c8a7a721f5
Rodrigo Alves  Brito <rodrigoab123@gmail.com>
2022-06-10 12:21:53 -0300

    test(memcmp): ensure compares even through nulls

--- tests/srcs/test_ft_memcmp.c
+++ tests/srcs/test_ft_memcmp.c
@@ -19,11 +19,21 @@ void	test_memory_area_are_not_equal(void)
 	ASSERT_EXPR(result == expected);
 }
 
+void	test_memory_area_with_nulls(void)
+{
+	char	s1[] = {0, 1, 0, 1};
+	char	s2[] = {0, 1, 0, 0};
+	int		result = ft_memcmp(s1, s2, 4);
+	int		expected = memcmp(s1, s2, 4);
+	ASSERT_EXPR(result == expected);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_memory_area_are_equal),
 		UNIT_TEST(test_memory_area_are_not_equal),
+		UNIT_TEST(test_memory_area_with_nulls),
 	};
 	return RUN_GROUP(tests);
 }
