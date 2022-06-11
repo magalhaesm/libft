fa1a0c96fa8369ee5846693ccc3fc39bb44336b3
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-11 09:13:02 -0300

    feat: add ft_calloc

--- libft/libft.h
+++ libft/libft.h
@@ -6,14 +6,14 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 22:41:44 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/06/09 15:55:39 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/06/10 13:06:33 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stddef.h>
+# include <stdlib.h>
 
 typedef unsigned char	t_byte;
 
@@ -38,5 +38,6 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *nptr);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
