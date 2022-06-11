756836ba991d548b3ce7c545112a1ff851700bda
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-11 09:48:39 -0300

    test: check size of the allocated memory

--- tests/srcs/test_ft_calloc.c
+++ tests/srcs/test_ft_calloc.c
@@ -1,4 +1,5 @@
 #include "../utils/tester.h"
+#include <malloc.h>
 
 void	test_return_null_when_size_or_nmemb_is_zero(void)
 {
@@ -15,11 +16,20 @@ void	test_return_null_when_occurs_an_integer_overflow(void)
 	free(result);
 }
 
+void	test_check_size_of_the_allocated_memory_block(void)
+{
+	size_t	*result = ft_calloc(4, 2);
+	size_t	*expected = calloc(4, 2);
+	ASSERT_EXPR(malloc_usable_size(result) == malloc_usable_size(expected));
+	free(result);
+	free(expected);
+}
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_return_null_when_size_or_nmemb_is_zero),
 		UNIT_TEST(test_return_null_when_occurs_an_integer_overflow),
+		UNIT_TEST(test_check_size_of_the_allocated_memory_block),
 	};
 	return RUN_GROUP(tests);
 }
