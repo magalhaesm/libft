2a7be7717145709c2272e64ca33321c529b02d92
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-12 12:19:09 -0300

    test: ensure that ft_strjoin allocates only the space needed

--- tests/srcs/test_ft_strjoin.c
+++ tests/srcs/test_ft_strjoin.c
@@ -1,5 +1,6 @@
 #include "../utils/tester.h"
 #include <string.h>
+#include <malloc.h>
 
 void	test_return_a_new_joined_string(void)
 {
@@ -21,11 +22,28 @@ void	test_join_an_empty_string(void)
 	free(result);
 }
 
+void	test_allocated_size(void)
+{
+	char	*s1 = "Lorem ";
+	char	*s2 = "ipsum.";
+	char	*expected_str = "Lorem ipsum.";
+	char	*result = ft_strjoin(s1, s2);
+	char	*size_needed = malloc(strlen(expected_str) + 1);
+
+	int		allocated = malloc_usable_size(result);
+	int		expected = malloc_usable_size(size_needed);
+
+	ASSERT_EXPR(allocated == expected);
+	free(result);
+	free(size_needed);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_return_a_new_joined_string),
 		UNIT_TEST(test_join_an_empty_string),
+		UNIT_TEST(test_allocated_size),
 	};
 	return RUN_GROUP(tests);
 }
