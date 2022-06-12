d06a424f67f68a14e8355942fe3c76229aa810c9
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-12 11:07:15 -0300

    test: ensure ft_substr return just a char

--- tests/srcs/test_ft_substr.c
+++ tests/srcs/test_ft_substr.c
@@ -11,10 +11,20 @@ void	test_return_a_substring(void)
 	free(result);
 }
 
+void	test_return_just_a_char_in_a_string(void)
+{
+	char	*string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+	char	*result = ft_substr(string, 1, 1);
+	char	*expected = "o";
+	ASSERT_EXPR(!strcmp(result, expected) && (result != expected));
+	free(result);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_return_a_substring),
+		UNIT_TEST(test_return_just_a_char_in_a_string),
 	};
 	return RUN_GROUP(tests);
 }
