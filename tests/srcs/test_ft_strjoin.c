05978578b240a0f9955980a7bd11baf31a22557e
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-12 12:14:40 -0300

    test: ensure that ft_strjoin joins empty strings

--- tests/srcs/test_ft_strjoin.c
+++ tests/srcs/test_ft_strjoin.c
@@ -11,10 +11,21 @@ void	test_return_a_new_joined_string(void)
 	free(result);
 }
 
+void	test_join_an_empty_string(void)
+{
+	char	*s1 = "";
+	char	*s2 = "ipsum.";
+	char	*result = ft_strjoin(s1, s2);
+	char	*expected = "ipsum.";
+	ASSERT_EXPR(!strcmp(result, expected));
+	free(result);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_return_a_new_joined_string),
+		UNIT_TEST(test_join_an_empty_string),
 	};
 	return RUN_GROUP(tests);
 }
