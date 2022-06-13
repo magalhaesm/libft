e4383b24e69c84fc7d89aa29ac53a0b74ef18851
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-13 13:23:58 -0300

    test(ft_strncmp): improve test

--- tests/srcs/test_ft_strncmp.c
+++ tests/srcs/test_ft_strncmp.c
@@ -5,36 +5,28 @@ void	test_ascii_string_are_equal(void)
 {
 	char	*s1 = "ABC";
 	char	*s2 = "AB";
-	int		result = ft_strncmp(s1, s2, 2);
-	int		expected = strncmp(s1, s2, 2);
-	ASSERT_EXPR(result == expected);
+	ASSERT_EXPR(ft_strncmp(s1, s2, 2) == 0);
 }
 
 void	test_ascii_string_are_different(void)
 {
 	char	*s1 = "ABC";
 	char	*s2 = "AB";
-	int		result = ft_strncmp(s1, s2, 3);
-	int		expected = strncmp(s1, s2, 3);
-	ASSERT_EXPR(result == expected);
+	ASSERT_EXPR(ft_strncmp(s1, s2, 3) > 0);
 }
 
 void	test_non_ascii_string_are_different(void)
 {
 	char	s1[] = {'A', 'B', -10};
 	char	*s2 = "AB";
-	int		result = ft_strncmp(s1, s2, 3);
-	int		expected = strncmp(s1, s2, 3);
-	ASSERT_EXPR(result == expected);
+	ASSERT_EXPR(ft_strncmp(s1, s2, 3) > 0);
 }
 
 void	test_return_zero_when_n_is_zero(void)
 {
 	char	s1[] = {'A', 'B', 10};
 	char	*s2 = "AB";
-	int		result = ft_strncmp(s1, s2, 0);
-	int		expected = strncmp(s1, s2, 0);
-	ASSERT_EXPR(result == expected);
+	ASSERT_EXPR(ft_strncmp(s1, s2, 0) == 0);
 }
 
 int	main(void)
