7c2ea530ae8158558630cea130b420888bb12e47
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-13 13:17:58 -0300

    test(ft_atoi): improve readability

--- tests/srcs/test_ft_atoi.c
+++ tests/srcs/test_ft_atoi.c
@@ -1,53 +1,44 @@
 #include "../utils/tester.h"
 #include <stdlib.h>
 
-typedef struct	s_test
-{
-	char	*value;
-	int	result;
-	int	expected;
-}	t_test;
-
-t_test t;
-
 void	test_convert_negative_signed_integers(void)
 {
-	t.value = "-42";
-	t.result = ft_atoi(t.value);
-	t.expected = atoi(t.value);
-	ASSERT_EXPR(t.result == t.expected);
+	char	*value = "-42";
+	int		result = ft_atoi(value);
+	int 	expected = atoi(value);
+	ASSERT_EXPR(result == expected);
 }
 
 void	test_convert_positive_signed_integers(void)
 {
-	t.value = "+42";
-	t.result = ft_atoi(t.value);
-	t.expected = atoi(t.value);
-	ASSERT_EXPR(t.result == t.expected);
+	char	*value = "+42";
+	int		result = ft_atoi(value);
+	int 	expected = atoi(value);
+	ASSERT_EXPR(result == expected);
 }
 
 void	test_convert_positive_unsigned_integers(void)
 {
-	t.value = "42";
-	t.result = ft_atoi(t.value);
-	t.expected = atoi(t.value);
-	ASSERT_EXPR(t.result == t.expected);
+	char	*value = "42";
+	int		result = ft_atoi(value);
+	int 	expected = atoi(value);
+	ASSERT_EXPR(result == expected);
 }
 
 void	test_is_space(void)
 {
-	t.value = "  \t\n\v\f\r+42";
-	t.result = ft_atoi(t.value);
-	t.expected = atoi(t.value);
-	ASSERT_EXPR(t.result == t.expected);
+	char	*value = "  \t\n\v\f\r+42";
+	int		result = ft_atoi(value);
+	int 	expected = atoi(value);
+	ASSERT_EXPR(result == expected);
 }
 
 void	test_convert_min_integer_value(void)
 {
-	t.value = "-2147483648";
-	t.result = ft_atoi(t.value);
-	t.expected = atoi(t.value);
-	ASSERT_EXPR(t.result == t.expected);
+	char	*value = "-2147483648";
+	int		result = ft_atoi(value);
+	int 	expected = atoi(value);
+	ASSERT_EXPR(result == expected);
 }
 
 int	main(void)
