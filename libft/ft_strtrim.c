88cfb3472b272ef791e96998f84c35f1c2e79581
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-13 12:47:44 -0300

    test: ensure that ft_strtrim trim all string

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,30 +6,28 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 12:21:17 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/06/12 16:51:27 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/06/13 12:44:42 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*start;
-	char	*end;
 	char	*trimmed;
 	size_t	len;
 
+	while (ft_strchr(set, *s1) && *s1)
+		s1++;
 	len = ft_strlen(s1);
-	start = (char *)s1;
-	end = (char *)s1 + len;
-	while (ft_strchr(set, *start))
-		start++;
-	while (ft_strchr(set, *end))
-		end--;
-	len = end - start + 2;
-	trimmed = malloc(len);
-	if (!trimmed)
-		return (NULL);
-	ft_strlcpy(trimmed, start, len);
-	return (trimmed);
+	while (ft_strchr(set, s1[len]))
+		len--;
+	trimmed = malloc(len + 2);
+	if (trimmed)
+	{
+		ft_strlcpy(trimmed, s1, len + 2);
+		return (trimmed);
+	}
+	return (ft_strdup(""));
 }
--- tests/srcs/test_ft_strtrim.c
+++ tests/srcs/test_ft_strtrim.c
@@ -11,10 +11,21 @@ void	test_return_a_trimmed_string(void)
 	free(result);
 }
 
+void	test_trim_all_string(void)
+{
+	char	*string = "lorem ipsum";
+	char	*set = "lorem ipsum";
+	char	*result = ft_strtrim(string, set);
+	char	*expected = "";
+	ASSERT_EXPR(!strcmp(result, expected));
+	free(result);
+}
+
 int	main(void)
 {
 	t_unit_test tests[] = {
 		UNIT_TEST(test_return_a_trimmed_string),
+		UNIT_TEST(test_trim_all_string),
 	};
 	return RUN_GROUP(tests);
 }
