5224a440e0d3db06a6c823ce9189513a89fa64a4
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-21 15:22:32 -0300

    test(putendl_fd): ensure output to file descriptor

---
+++ tests/srcs/test_ft_putendl_fd.c
@@ -0,0 +1,18 @@
+#include "../utils/tester.h"
+#include <string.h>
+
+void	test_output_to_fd(void)
+{
+	int	fd = mktmpfd();
+	ft_putendl_fd("lorem ipsum dolor sit amet", fd);
+	char	*result = get_output(fd);
+	ASSERT_EXPR(!strcmp(result, "lorem ipsum dolor sit amet\n"));
+}
+
+int	main(void)
+{
+	t_unit_test tests[] = {
+		UNIT_TEST(test_output_to_fd),
+	};
+	return RUN_GROUP(tests);
+}
