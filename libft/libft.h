85a6379e12cbf2f6027e644a500bbbe4a82f938a
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-06-21 10:28:44 -0300

    feat: add ft_lstadd_back

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mdias-ma <mdias-ma@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 22:41:44 by mdias-ma          #+#    #+#             */
-/*   Updated: 2022/06/21 10:28:06 by mdias-ma         ###   ########.fr       */
+/*   Updated: 2022/06/21 10:28:23 by mdias-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,6 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *newn);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *newn);
 
 #endif
